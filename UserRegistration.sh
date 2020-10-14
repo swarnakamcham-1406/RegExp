62b5504d566a9d9947de617341326ccec043bf37
kamchamswarna <kamchamswarna@gmail.com>
2020-10-14 10:36:21 +0530

    check employee firstName

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,16 @@
-
+#! /bin/bash -x
 echo "welcome to the User Registration Form"
 
+echo "enter the firstName"
+
+read value
+firstName="^(?=(.*[A-Z]){1})[a-z]{2,15}"
+if [[ $value =~ $firstName ]]
+then
+
+echo enter valid UserName;
+
+else
+ echo you enterd invalid userName;
+
+fi
